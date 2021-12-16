77ab60dbf6529822ecbfda8f6703523c43862714
Shayan Nasrabadi <williams.ca001@gmail.com>
2021-12-16 14:40:25 +0330

    Config Updated

--- functions/config.js
+++ functions/config.js
@@ -6,20 +6,32 @@ module.exports = {
   description: "Config database",
   execute(client) {
     console.log("[DB] Checking DataBase...");
-    if (!db.has("bot.token")) {
+
+    if (!db.has("bot.token") && config.bot.token) {
       db.set("bot.token", config.bot.token);
       console.log("[DB] Token registered in database");
     }
 
-    if (!db.has("bot.prefix")) {
+    if (!db.has("bot.prefix") && config.bot.prefix) {
       db.set("bot.prefix", config.bot.prefix);
       console.log(`[DB] Prefix registered in database`);
     }
 
-    if (!db.has("bot.colors.main")) {
+    if (!db.has("bot.colors.main") && config.colors.main) {
       db.set("bot.colors.main", config.colors.main);
       console.log(`[DB] Main color registered in database`);
     }
+
+    if (!db.has("bot.colors.log") && config.colors.log) {
+      db.set("bot.colors.log", config.colors.log);
+      console.log(`[DB] Log color registered in database`);
+    }
+
+    if (!db.has("embeds.footer") && config.embeds.footer) {
+      db.set("embeds.footer", config.embeds.footer);
+      console.log(`[DB] Footer text registered in database`);
+    }
+
     console.log("[DB] DataBase checked");
   },
 };
